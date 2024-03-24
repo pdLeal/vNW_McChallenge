a26f209baf01b6b33d74b6235325d7aefc564e68
pdLeal <lealpd@outlook.com>
2024-03-24 10:16:21 -0300

    feat: header and its style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import React from "react";
+import Header from "./components/Header";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, *::before, *::after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  max-width: 100%;
+  vertical-align: middle;
+  font-style: italic;
+}
+
+:root {
+  --small-size: 4px;
+  --default-size: 8px;
+  --size-2x: 16px;
+  --size-3x: 24px;
+  --size-4x: 32px;
+}
+`;
+
 function App() {
 
   return (
     <>
-      <h1>Oi</h1>
+      <GlobalStyle />
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../imgs/logo.png';
+import Cel1 from '../imgs/app.png';
+import Cel2 from '../imgs/app2.png';
+
+const Styled_Header = styled.header`
+  padding: var(--size-2x) var(--size-4x);
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  gap: var(--size-2x);
+      `;
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  row-gap: var(--small-size);
+  column-gap: var(--size-4x);
+
+    & > * {
+      padding-block: 0.1em;
+      padding-inline: var(--size-2x);
+      border-radius: var(--default-size);
+      }
+
+    @media only screen and (max-width: 515px) {
+      max-width: 200px;
+    }
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  font-size: clamp(0.8rem, 2vw + 0.5rem, 1.3rem);
+  color: black;
+  display: flex;
+  align-items: center;
+
+    & :first-child { // Selects the first child of each link
+      margin-right: var(--default-size);
+    }
+
+    &:nth-child(1) {
+        &:hover {
+          background-color: #fff0c8;
+        }
+
+        &:active {
+          font-weight: bold;
+        }
+    }
+
+    &:nth-child(2) { // Selects the link that's a second child
+      font-weight: bold;
+      background-color: #FFC72C;
+
+        &:hover {
+          background-color: #fcb900;
+        }
+
+        &:active {
+          background-color: #a27700;
+        }
+    }
+    
+    img {
+      width: clamp(20px, 2vw + 1rem, 30px);
+    }
+`;
+
+
+function Header() {
+  return (
+    <Styled_Header>
+      <a href='#'><img src={Logo} alt="Logo do Mc Donalds" /></a>
+      <Div>
+        <Link href='#'>
+          <img src={Cel1} alt="Icone de celular minimalista" />
+          <p>Baixe o App</p>
+        </Link>
+        <Link href='#'>
+          <p>Peça seu Méqui</p>
+          <img src={Cel2} alt="Celular com um saco de lanches do Mc Donalds na tela" />
+        </Link>
+      </Div>
+    </Styled_Header>
+  )
+}
+
+export default Header
